21b35044a3fcabd0529ffe6bdbadefe8ad1d56c8
flavtor <flavtor@localhost.localdomain>
2020-11-29 15:05:25 -0500

    [FIX] add -h

--- B-MUL-100-PAR-1-1-myhunter-flavien.thel/main.c
+++ B-MUL-100-PAR-1-1-myhunter-flavien.thel/main.c
@@ -9,6 +9,17 @@
 #include "hunter.h"
 #include "struct.h"
 
+static void usage_desc(void)
+{
+    my_putstr("USAGE\n\t./my_hunter\n\n");
+    my_putstr("DESCRIPTION\n\t");
+    my_putstr("A small video game based on the rules of Duck Hunt\n\n\t");
+    my_putstr("The basic rules for the my_hunter are as follows :\n\n\t\t");
+    my_putstr("*The playser is a hunter who shoots pokemon.\n\t\t");
+    my_putstr("*pokemon must appear on the screen and move from one side.\n\t");
+    my_putstr("\t*the player can click on them to shoot them.\n\n");
+}
+
 static int modif_strcmp(char const *s1, char const *s2)
 {
     int i = 0;
@@ -34,6 +45,8 @@ int main(int ac, char **av, char **env)
     sfVideoMode mode = {1920, 1080, 60};
     sfRenderWindow *w;
 
+    if (ac == 2 && av[1][0] == '-' && av[1][1] == 'h')
+        usage_desc(); 
     if (ac != 1 || av[0] == NULL)
         return (84);
     if (verif_env(env) == -1)
